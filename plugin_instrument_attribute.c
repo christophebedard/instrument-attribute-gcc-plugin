05b966542f2ab9fad4c262080f081fae9afd3974
Christophe Bedard <bedard.christophe@gmail.com>
2019-07-27 18:19:23 +0200

    Add info

--- plugin_instrument_attribute.c
+++ plugin_instrument_attribute.c
@@ -5,10 +5,21 @@
 
 int plugin_is_GPL_compatible;
 
+static struct plugin_info info = {
+    "0.0.1",
+    "This plugin provides the instrument_function attribute.",
+};
+
 int plugin_init(
     struct plugin_name_args * plugin_info,
     struct plugin_gcc_version * version)
 {
     std::cout << "Plugin: instrument_function attribute" << std::endl;
+
+    register_callback(
+        plugin_info->base_name,
+        PLUGIN_INFO,
+        NULL,
+        &info);
     return 0;
 }

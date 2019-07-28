5c4d95f25e32a7e0db57f5784e3737387b02fce2
Christophe Bedard <bedard.christophe@gmail.com>
2019-07-28 15:08:56 +0200

    Add useful #define for the attribute

--- test.c
+++ test.c
@@ -1,6 +1,8 @@
 #include <iostream>
 
-void __attribute__((instrument_function)) an_instrumented_function()
+#define INSTRUMENT_FUNCTION __attribute__((instrument_function))
+
+void INSTRUMENT_FUNCTION an_instrumented_function()
 {
     std::cout << "this is instrumented" << std::endl;
 }
@@ -10,7 +12,7 @@ void not_instrumented_function()
     std::cout << "this is not instrumented" << std::endl;
 }
 
-int main()
+int INSTRUMENT_FUNCTION main()
 {
     std::cout << "an_instrumented_function()" << std::endl;
     an_instrumented_function();

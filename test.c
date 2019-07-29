9450defcccda5be174983862c09a21008191dd7a
Christophe Bedard <bedard.christophe@gmail.com>
2019-07-29 20:01:23 +0200

    Simplify test.c

--- test.c
+++ test.c
@@ -1,23 +1,18 @@
 #include <stdio.h>
 
-#define INSTRUMENT_FUNCTION __attribute__((instrument_function))
-
-void INSTRUMENT_FUNCTION an_instrumented_function()
+void __attribute__((instrument_function)) instrumented_function()
 {
     printf("this is instrumented\n");
 }
 
-void not_instrumented_function()
+void NOT_instrumented_function()
 {
     printf("this is not instrumented");
 }
 
-int INSTRUMENT_FUNCTION main()
+int __attribute__((instrument_function)) main()
 {
-    printf("an_instrumented_function()");
-    an_instrumented_function();
-
-    printf("not_instrumented_function()");
-    not_instrumented_function();
+    instrumented_function();
+    NOT_instrumented_function();
     return 0;
 }

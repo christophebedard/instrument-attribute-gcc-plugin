9541c83f886567705899bac9dddfc34595a892dc
Christophe Bedard <bedard.christophe@gmail.com>
2021-05-13 19:56:22 -0400

    Extract utils from main source file
    Signed-off-by: Christophe Bedard <bedard.christophe@gmail.com>

--- Makefile
+++ Makefile
@@ -1,29 +1,38 @@
 CXX=g++
 TARGET_GCC=gcc
 PLUGIN_NAME= instrument_attribute
-PLUGIN_SOURCE_FILES= $(PLUGIN_NAME).c
+PLUGIN_SOURCE_FILES= src/$(PLUGIN_NAME).c
+PLUGIN_INCLUDE_DIR= include
+UTILS_SOURCE_FILES= src/utils.c
+UTILS_HEADER_FILES= $(PLUGIN_INCLUDE_DIR)/utils.h
 TEST_SOURCE_FILES= test/test.c test/other/other_file.h
 GCCPLUGINS_DIR:= $(shell $(TARGET_GCC) -print-file-name=plugin)
-CXXFLAGS+= -I$(GCCPLUGINS_DIR)/include -fPIC -O2
+CXX_FLAGS_BASE= -fPIC -O2
+CXXFLAGS+= -I$(GCCPLUGINS_DIR)/include $(CXX_FLAGS_BASE)
 
 all: $(PLUGIN_NAME).so
 
-$(PLUGIN_NAME).so: $(PLUGIN_SOURCE_FILES)
-	$(CXX) -shared $(CXXFLAGS) $^ -o $@
+utils.o: $(UTILS_SOURCE_FILES) $(UTILS_HEADER_FILES)
+	$(CXX) $(CXX_FLAGS_BASE) -I$(PLUGIN_INCLUDE_DIR) $< -c -o $@
+
+$(PLUGIN_NAME).so: $(PLUGIN_SOURCE_FILES) $(UTILS_HEADER_FILES) utils.o
+	$(CXX) -shared $(CXXFLAGS) -I$(PLUGIN_INCLUDE_DIR) $^ -o $@
 
 clean:
+	rm -f utils.o
 	rm -f $(PLUGIN_NAME).so
-	rm -f test/test
 	rm -rf test-trace/
 
 verify: $(PLUGIN_NAME).so
 	$(CXX) $(CXXFLAGS) -fplugin=./$(PLUGIN_NAME).so -c -x c++ /dev/null -o /dev/null
 
-test: ${TEST_SOURCE_FILES} $(PLUGIN_NAME).so
+test: $(TEST_SOURCE_FILES) $(PLUGIN_NAME).so
 	VERBOSE=1 $(TARGET_GCC) -fplugin=./$(PLUGIN_NAME).so -finstrument-functions \
-		-fplugin-arg-instrument_attribute-include-file-list=/something/else,test/other \
+		-fplugin-arg-instrument_attribute-include-file-list=test/other/other_file3.h,test/other/other_file.h \
 		-fplugin-arg-instrument_attribute-include-function-list=instrumented_with_function_list,random_other_function_name \
 		$< -o test/$@
+
+test_all: verify test
 
 trace: test
 	chmod +x trace.sh
---
+++ include/utils.h
@@ -0,0 +1,50 @@
+// Copyright (C) 2021 Christophe Bedard
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/// Dynamic strings list.
+struct string_list {
+  /// String array.
+  char ** data;
+  /// Number of strings in the array.
+  size_t len;
+  /// Actual length of the array, could be greater than len.
+  size_t data_len;
+};
+
+/// Custom strdup.
+/**
+ * Not poisoned.
+ */
+char * strdup_(const char * str);
+
+/// Count the number of occurrences of the given character in the given string.
+size_t count(const char * str, const char * character);
+
+/// Split string into list using given separator.
+void split_str(char * str, const char * sep, struct string_list * list);
+
+/// Free and reset list.
+void free_list(struct string_list * list);
+
+/// Check if a string in the list is a substring of the given string
+/**
+ * \param list the list
+ * \param str1 the string
+ * \return the match (const string) from the list, or NULL.
+ */
+const char * list_strstr(struct string_list * list, const char * str1);
